4a5ed1f0d1ce80a1839c4d57dfa78681e0932885
Jaroslav Vehovský <jaroslav.vehovsky@gmail.com>
2024-04-22 02:34:08 +0200

    refactor: extract code from main into separate methods

--- Program.cs
+++ Program.cs
@@ -1,51 +1,32 @@
-﻿using System.Net.NetworkInformation;
-using CommandLine;
-using CommandLine.Text;
-using Microsoft.VisualBasic;
-using SharpPcap;
+﻿using CommandLine;
 
 namespace IPK_Project2;
 
 static class Program {
 	static void Main(string[] args) {
 		// Handle CTRL+C
-		Console.CancelKeyPress += new ConsoleCancelEventHandler(HandleCancelEvent);
+		Console.CancelKeyPress += HandleCancelEvent;
 
 		// Parse arguments
 		ParserResult<Cli> cli = Parser.Default.ParseArguments<Cli>(args);
-		cli.WithNotParsed(o => { Environment.Exit(0); });
+		cli.WithNotParsed(_ => { Environment.Exit(0); });
+		// Validate arguments
 		cli.WithParsed(o => {
-			if (o is { Port: not null, PortDestination: not null } or { Port: not null, PortSource: not null } or { PortDestination: not null, PortSource: not null }) {
-				Error.Exit("Invalid combination of arguments. -p --port-destination and --port-source cannot be used in combination");
+			if (o is { Port: not null, PortDestination: not null } or { Port: not null, PortSource: not null } or
+			    { PortDestination: not null, PortSource: not null }) {
+				Error.Exit(
+					"Invalid combination of arguments. -p --port-destination and --port-source cannot be used in combination");
 			}
 
-			if ((o.Port != null || o.PortDestination != null || o.PortSource != null) && o is { Tcp: false, Udp: false }) {
-				Error.Exit("Invalid combination of arguments. -p --port-destination and --port-source have to be used in combination with --tcp or --udp");
+			if ((o.Port != null || o.PortDestination != null || o.PortSource != null) &&
+			    o is { Tcp: false, Udp: false }) {
+				Error.Exit(
+					"Invalid combination of arguments. -p --port-destination and --port-source have to be used in combination with --tcp or --udp");
 			}
 		});
 
-		// Print available interfaces if the value for -i or --interface was not specified or no argument were provided
-		if (cli.Value.Interface == null || args.Length == 0) {
-			try {
-				Interface.ListNetworkInterfaces();
-			} catch (Exception e) {
-				Error.Exit(e.Message);
-			}
-
-			Environment.Exit(0);
-		}
-
-		// Handle invalid interface
-		if (!Interface.InterfaceExists(cli.Value.Interface)) {
-			Error.Print($"Interface {cli.Value.Interface} does not exist");
-			try {
-				Interface.ListNetworkInterfaces();
-			} catch (Exception e) {
-				Error.Exit(e.Message);
-			}
-
-			Environment.Exit(1);
-		}
+		// Validate interface
+		CheckInterface(cli.Value);
 
 		try {
 			Sniffer.Sniff(cli.Value);
@@ -55,9 +36,28 @@ static class Program {
 	}
 
 	// Handle CTRL+C
-	static void HandleCancelEvent(object? sender, ConsoleCancelEventArgs eventArgs) {
+	private static void HandleCancelEvent(object? sender, ConsoleCancelEventArgs eventArgs) {
 		eventArgs.Cancel = true;
 		Sniffer.Close();
 		eventArgs.Cancel = false;
+	}
+	
+	// Print available interfaces if the value for -i or --interface was not specified or the interface does nto exist
+	private static void CheckInterface(Cli args) {
+		try {
+			if (args.Interface == null) {
+				Interface.ListNetworkInterfaces();
+				Environment.Exit(0);
+			}
+
+			if (!Interface.InterfaceExists(args.Interface)) {
+				Error.Print($"Interface {args.Interface} does not exist");
+				Interface.ListNetworkInterfaces();
+				Environment.Exit(1);
+			}
+			
+		} catch (Exception e) {
+			Error.Exit(e.Message);
+		}
 	}
 }
