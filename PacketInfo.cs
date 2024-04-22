720aacd6b7432a5a9d59b1d2d36a14e25b3eb085
Jaroslav Vehovský <jaroslav.vehovsky@gmail.com>
2024-04-22 03:43:12 +0200

    refactor: split packet parsing and printing into multiple functions

--- PacketInfo.cs
+++ PacketInfo.cs
@@ -19,40 +19,54 @@ public class PacketInfo {
 
 	// Convert packet into object
 	public void PacketToObject(PacketCapture packetCapture) {
-		Packet = PacketDotNet.Packet.ParsePacket(packetCapture.GetPacket().LinkLayerType,
+		Packet = Packet.ParsePacket(packetCapture.GetPacket().LinkLayerType,
 			packetCapture.GetPacket().Data);
 
-		var ethPacket = (EthernetPacket)Packet;
-		
-		// Do not extract IP information from ARP packets
-		EthernetType = ethPacket.Type;
-		if (EthernetType == EthernetType.IPv4 || EthernetType == EthernetType.IPv6) {
-			var ipPacket = (IPPacket)Packet.Extract<IPPacket>();
-			
-			Protocol = ipPacket.Protocol;
-			if (Protocol == ProtocolType.Tcp) {
-				var tpcPacket = (TcpPacket)Packet.Extract<TcpPacket>();
-				
-				SourcePort = tpcPacket.SourcePort;
-				DestinationPort = tpcPacket.DestinationPort;
-			} else if (Protocol == ProtocolType.Udp) {
-				var udpPacket = (UdpPacket)Packet.Extract<UdpPacket>();
-				
-				SourcePort = udpPacket.SourcePort;
-				DestinationPort = udpPacket.DestinationPort;
-			}
-			
-			SourceIp = ipPacket.SourceAddress.ToString();
-			DestinationIp = ipPacket.DestinationAddress.ToString();
+		ParseEthernetPacket();
+
+		if (EthernetType is EthernetType.IPv4 or EthernetType.IPv6) {
+			ParseIpPacket();
 		}
-		
+
 		Timestamp = packetCapture.Header.Timeval.Date.ToLocalTime().ToString("s");
-		SourceMac = FormatMacAddress(ethPacket.SourceHardwareAddress);
-		DestinationMac = FormatMacAddress(ethPacket.DestinationHardwareAddress);
 		FrameLength = packetCapture.Data.Length;
 	}
 
-	// Print packet information to the console
+	// Extract information from ethernet frame
+	private void ParseEthernetPacket() {
+		var ethPacket = (EthernetPacket)Packet!;
+		EthernetType = ethPacket.Type;
+		SourceMac = FormatMacAddress(ethPacket.SourceHardwareAddress);
+		DestinationMac = FormatMacAddress(ethPacket.DestinationHardwareAddress);
+	}
+
+	// Extract information from IP packet
+	private void ParseIpPacket() {
+		var ipPacket = Packet!.Extract<IPPacket>();
+
+		Protocol = ipPacket.Protocol;
+		switch (Protocol) {
+			case ProtocolType.Tcp: {
+				var tpcPacket = Packet.Extract<TcpPacket>();
+
+				SourcePort = tpcPacket.SourcePort;
+				DestinationPort = tpcPacket.DestinationPort;
+				break;
+			}
+			case ProtocolType.Udp: {
+				var udpPacket = Packet.Extract<UdpPacket>();
+
+				SourcePort = udpPacket.SourcePort;
+				DestinationPort = udpPacket.DestinationPort;
+				break;
+			}
+		}
+
+		SourceIp = ipPacket.SourceAddress.ToString();
+		DestinationIp = ipPacket.DestinationAddress.ToString();
+	}
+
+	// Print the packet to console
 	public void PrintPacket(int currentPacket, int max) {
 		// Print separator
 		if (currentPacket > 1) {
@@ -61,67 +75,79 @@ public class PacketInfo {
 			Console.WriteLine();
 		}
 
-		if (max > 1) {
-			Console.WriteLine($"Packet {currentPacket}/{max}");
-		}
-		
+		Console.WriteLine($"Packet {currentPacket}/{max}");
+
+		PrintGeneralPacketInformation();
+
+		PrintPacketHexDump();
+	}
+
+	private void PrintGeneralPacketInformation() {
 		Console.WriteLine($"timestamp: {Timestamp}");
 		Console.WriteLine($"src MAC: {SourceMac}");
 		Console.WriteLine($"dst MAC: {DestinationMac}");
 		Console.WriteLine($"frame length: {FrameLength} bytes");
-		if (EthernetType == EthernetType.IPv4 || EthernetType == EthernetType.IPv6) {
+		if (EthernetType is EthernetType.IPv4 or EthernetType.IPv6) {
 			Console.WriteLine($"src IP: {SourceIp}");
 			Console.WriteLine($"dst IP: {DestinationIp}");
-			
-			if (Protocol == ProtocolType.Tcp || Protocol == ProtocolType.Udp) {
+
+			if (Protocol is ProtocolType.Tcp or ProtocolType.Udp) {
 				Console.WriteLine($"src port: {SourcePort}");
 				Console.WriteLine($"dst port: {DestinationPort}");
 			}
-			
+
 			Console.WriteLine($"protocol: {Protocol.ToString()}");
 		}
+
 		Console.WriteLine($"ethernet type: {EthernetType.ToString()}");
 		Console.WriteLine();
+	}
 
-		// 0x0000: 5c a6 e6 1a 85 f0 2c c8  1b 0a a7 fe 08 00 45 00  \¦æ..ð,È..§þ..E.
-		// 0x0010: 00 ec 5d c3 40 00 3c 06  9b 2e a2 9f 88 ea c0 a8  .ì]Ã@.<...¢..êÀ¨
+	private void PrintPacketHexDump() {
 		if (Packet == null) {
 			return;
 		}
 
 		var bytes = Packet.Bytes;
 		for (int i = 0; i < bytes.Length; i += 16) {
-			// Left size
+			// Left side
 			Console.Write($"0x{i:x4}: ");
-
-			char[] chars = new char[16];
-			// 2x 16 per line
-			for (int j = 0; j < 16; j++) {
-				if (i + j < bytes.Length) {
-					Console.Write($"{bytes[i + j]:x2} ");
-					// Non-printable characters are replaced with '.'
-					char c = (char)bytes[i + j];
-					chars[j] = char.IsControl(c) ? '.' : c;
-				} else {
-					// Fill the last row if it's less than 16 bytes
-					Console.Write("   ");
-					chars[j] = ' ';
-				}
-
-				// Every 8 byted add space
-				if (j == 7) {
-					Console.Write(" ");
-				}
-			}
-
-			Console.Write(" ");
+			char[] chars = FormatHexLine(bytes, i);
 			// Right side
+			Console.Write(" ");
 			Console.WriteLine(chars);
 		}
 	}
 
+	private char[] FormatHexLine(byte[] bytes, int offset) {
+		char[] chars = new char[16];
+		// 2x 16 per line
+		for (int j = 0; j < 16; j++) {
+			chars[j] = FormatByte(bytes, offset + j);
+			// Every 8 byted add space
+			if (j == 7) {
+				Console.Write(" ");
+			}
+		}
+
+		return chars;
+	}
+
+	private char FormatByte(byte[] bytes, int index) {
+		if (index < bytes.Length) {
+			Console.Write($"{bytes[index]:x2} ");
+			// Non-printable characters are replaced with '.'
+			char c = (char)bytes[index];
+			return char.IsControl(c) ? '.' : c;
+		}
+		
+		// Fill the last row if it's less than 16 bytes
+		Console.Write("   ");
+		return ' ';
+	}
+
 	// Converts mac address into "00:1b:3f:56:8a:00" format
 	private static string FormatMacAddress(PhysicalAddress address) {
-		return String.Join(":", address.GetAddressBytes().Select(x => x.ToString("x2")).ToArray());
+		return string.Join(":", address.GetAddressBytes().Select(x => x.ToString("x2")).ToArray());
 	}
 }
